0ad853bb2d395cde3cf9ad489d4d1d5f3e495317
Duong <dhoa0014@student.monash.edu>
2026-03-14 13:24:13 +1100

    database update

---
+++ database.py
@@ -0,0 +1,19 @@
+# database.py
+import sqlite3
+
+DB_NAME = 'database.db'
+
+
+def get_connection():
+    return sqlite3.connect(DB_NAME)
+
+
+def init_db():
+    with get_connection() as conn:
+        conn.execute("""CREATE TABLE IF NOT EXISTS clothes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            category TEXT,   -- Hoodie, T-shirt, Short, Pants
+            color TEXT,
+            weather_type TEXT, -- Cold, Hot, Rainy
+            image_path TEXT
+        )""")
