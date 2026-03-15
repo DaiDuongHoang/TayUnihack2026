99d3008e77da6850e96136f5dafeb6ae01d8f273
Hoang Long Nguyen <hngu0269@student.monash.edu>
2026-03-15 15:52:13 +1100

    minor typo fix in "About Taylr"

--- Frontend/about.py
+++ Frontend/about.py
@@ -146,7 +146,7 @@ with center:
     st.space(10)
 
     st.markdown(
-        'Taylr is designed to take the guesswork out of getting dressed. By combining digital wardrobe organization with real-time weather context, it helps you make the most of the clothes you already own.'
+        '**Taylr** is designed to take the guesswork out of getting dressed. By combining digital wardrobe organization with real-time weather context, it helps you make the most of the clothes you already own.'
     )
 
     st.divider()
